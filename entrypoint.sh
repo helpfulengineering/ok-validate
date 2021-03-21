c0403fc49e81832e1333349c3cf91b6a27fd672e
psprings <psprings@users.noreply.github.com>
2021-03-21 09:53:24 -0400

    fix schema flag reference

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ else
     strict_mode='--no-strict'
     if [ "${INPUT_STRICT}" = "true" ]; then strict_mode=''; fi
     INPUT_CPU_NUM="$(env | sed -n 's/^INPUT_CPU-NUM=\(.*\)/\1/p')"
-    output=$(okv --schema=${INPUT_SCHEMA}  --cpu-num=${INPUT_CPU_NUM} --parser=${INPUT_PARSER} ${strict_mode} -p=${INPUT_PATH})
+    schema_flag="--ok=${INPUT_OKF}"
+    if [ ! -z "${INPUT_SCHEMA}" ]; then
+        schema_flag="--schema=${INPUT_SCHEMA}"
+    fi
+    output=$(okv ${schema_flag} --cpu-num=${INPUT_CPU_NUM} --parser=${INPUT_PARSER} ${strict_mode} -p=${INPUT_PATH})
     echo "::set-output name=results::$(echo $output)"
 fi
